7d0d0e89d030cf6e0d1005e0fef933e7b9202815
vivek chaturvedi <vc8bp3@gmail.com>
2024-09-24 17:44:39 +0530

    fixed build errors

--- app/contact/page.js
+++ app/contact/page.js
@@ -9,9 +9,9 @@ const data = [
 ]
 
 export const metadata = {
-    metadataBase : new URL(process.env.HOST),
+    metadataBase: new URL(process.env.HOST),
     title: "Contact Me | Get in Touch with Krishika Vyas",
-    description: "Have questions or want to collaborate? Reach out to Krishika Vyas, a digital marketing enthusiast. I'm here to help you with SEO, Meta ads, and social media strategies.",
+    description: "Have questions or want to collaborate? Reach out to Krishika Vyas, a digital marketing enthusiast. I&apos;m here to help you with SEO, Meta ads, and social media strategies.",
     keywords: ["contact", "get in touch", "digital marketing inquiries", "SEO questions", "Meta ads support", "social media marketing", "Krishika Vyas"],
     author: "Krishika",
     robots: "index, follow",
@@ -20,14 +20,13 @@ export const metadata = {
       type: "profile",
       url: new URL(process.env.HOST),
       images: [`${new URL(process.env.HOST)}/LOGOO.png`], 
-      description: "Have questions or want to collaborate? Reach out to Krishika Vyas, a digital marketing enthusiast. I'm here to help you with SEO, Meta ads, and social media strategies.",
+      description: "Have questions or want to collaborate? Reach out to Krishika Vyas, a digital marketing enthusiast. I&apos;m here to help you with SEO, Meta ads, and social media strategies.",
       card: "summary_large_image",
       locale: "en_US",
       siteName: "Krishika's portfolio",
-      site:"@krishika"
+      site: "@krishika"
     },
-  };
-  
+};
 
 function page() {
   return (
@@ -35,8 +34,8 @@ function page() {
         <Hero content="Contact" />
         <section className={style.details}>
             <div className={style.info}>
-                 <h2 className='commonContent' >Get in Touch <a><span>with Me!</span></a></h2>
-                 <p>Whether you have questions, want to collaborate, or are looking for guidance on marketing strategies, I'm here to help! Let's work together to bring your ideas to life and boost your brand's success.</p>
+                 <h2 className='commonContent'>Get in Touch <a><span>with Me!</span></a></h2>
+                 <p>Whether you have questions, want to collaborate, or are looking for guidance on marketing strategies, I&apos;m here to help! Let&apos;s work together to bring your ideas to life and boost your brand&apos;s success.</p>
             </div>
             <div className={style.cards}>
                 {data.map(e => (
@@ -47,7 +46,7 @@ function page() {
                 ))}
             </div>
         </section>
-        <Form/>
+        <Form />
     </div>
   )
 }
