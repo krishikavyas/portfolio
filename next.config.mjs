ce2a6a19a64ea6ae79a9be1601e6442f88f6d8ea
vivek chaturvedi <vc8bp3@gmail.com>
2024-10-13 15:15:36 +0530

    fixed redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,40 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-images: {
-        remotePatterns: [
+    images: {
+      remotePatterns: [
         {
-            protocol: 'https',
-            hostname: '**',
+          protocol: 'https',
+          hostname: '**',
         },
-        ],
+      ],
     },
-}
-
-export default nextConfig;
+    async redirects() {
+      return [
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'krishikavyas.site',
+            },
+          ],
+          destination: 'https://krishikavyas.site/:path*',
+          permanent: true,
+        },
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'www.krishikavyas.site',
+            },
+          ],
+          destination: 'https://krishikavyas.site/:path*',
+          permanent: true,
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
